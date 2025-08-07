84a914280908ea75431ee7efe04e63be8258f8d1
Marco Lecci <marco.lecci@italiaonline.it>
2025-08-07 12:36:02 +0200

    fix

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,7 +1,7 @@
 // webapp/src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app.tsx';
+import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './contexts/AuthContext.tsx';
